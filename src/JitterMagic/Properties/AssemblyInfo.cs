781442249a4377ecf12ea76fe782b33d4f1039ce
Jorrit Salverda <jorrit.salverda@gmail.com>
2015-08-07 10:21:02 +0200

    small textual updates

--- src/JitterMagic/Properties/AssemblyInfo.cs
+++ src/JitterMagic/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JitterMagic")]
-[assembly: AssemblyDescription("A c# library to add jitter to cache durations or any other values.")]
+[assembly: AssemblyDescription("A c# library to add jitter to cache durations or any other values")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("JitterMagic")]
